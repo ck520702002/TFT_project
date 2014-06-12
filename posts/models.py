b2f0a16d83796f109368474477dada96a2d4e59f
evansam119 <b00705016@ntu.edu.tw>
2014-06-12 17:08:46 +0800

    kevin told me to do

--- posts/models.py
+++ posts/models.py
@@ -5,9 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
 
 	title = models.CharField(max_length=10)
-	author = models.CharField(max_length=10)
 	context = models.CharField(max_length=500)
-	datetime = models.DateTimeField('post date') 
 	
 	def __unicode__(self):  # Python 3: def __str__(self):
 		return  self.title
