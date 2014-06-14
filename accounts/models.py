62ced69b66cef45aa01bd2b4d068e549bca41412
Kevin_Wu <ck520702002@ck520702002deMacBook-Pro.local>
2014-06-14 18:58:04 +0800

    兩個小時後的commit

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _  
 from userena.models import UserenaBaseProfile # or UserenaLanguageBaseProfile
   
+class Category(models.Model):
+	name = models.CharField(max_length=30)
+	description = models.CharField(max_length=30) 
+	def __unicode__(self):
+		return self.name
+
 class MyProfile(UserenaBaseProfile):
     user = models.OneToOneField(User,
     							unique=True,
@@ -13,6 +19,9 @@ class MyProfile(UserenaBaseProfile):
     description = models.CharField(_('description'),max_length=30) 
     school = models.CharField(max_length=30)       
     phone_number = models.CharField(max_length=30) 
-    account = models.CharField(max_length=30)       
+    account = models.CharField(max_length=30) 
+    category = models.ForeignKey(Category)
+
+     
 
 
