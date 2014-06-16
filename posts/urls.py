3acf7fc6a2f96b10f752b39995961c30d046f21a
evansam119 <b00705016@ntu.edu.tw>
2014-06-16 14:51:41 +0800

    ignore

--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from posts.views import PostView,ShowPost,PostDetail
-from posts import views
-
-urlpatterns = patterns('',
-	url(r'^$', PostView.as_view(success_url= "list" ), name="create" ),
-	url(r'^list/$', ShowPost.as_view()),
-	url(r'^list/(?P<pk>\d+)/detail/$', PostDetail.as_view())
- )
