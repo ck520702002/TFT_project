0682b0f73612ceed4342a53a502e365a86ede4cb
evansam119 <b00705016@ntu.edu.tw>
2014-06-27 20:39:33 +0800

    Revert "結合po文與文章列表"
    This reverts commit e98f81f7ad7630ee5c13f879ddae02ae4a0a6114.
    
    Conflicts:
    	db.sqlite3
    	posts/models.py
    	posts/urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -3,12 +3,9 @@ from posts.views import PostView,DiscussPost,PostDetail
 from posts import views
 
 urlpatterns = patterns('',
-<<<<<<< HEAD
+
 	url(r'^$', PostView.as_view(success_url= "/posts" ), name="create" ),
 	#url(r'^list/$', ShowPost.as_view()),
-=======
-	url(r'^$', PostView.as_view(success_url= "list" ), name="create" ),
-	url(r'^list/$', ShowPost.as_view()),
->>>>>>> parent of e98f81f... 結合po文與文章列表
+
 	url(r'^list/(?P<pk>\d+)/detail/$', PostDetail.as_view())
  )
