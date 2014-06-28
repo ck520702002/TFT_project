9159b447fd30783f70ac28248483bc771ff2ad04
jeanhuang78 <jean@jeantekiMacBook-Air.local>
2014-06-28 14:19:30 +0800

    我忘記這是啥了

--- TFT_project/urls.py
+++ TFT_project/urls.py
@@ -10,10 +10,12 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', IndexView.as_view(), name='main_base.html'),
     # url(r'^blog/', include('blog.urls')), 
+    url(r'^accounts/(?P<username>[\.\w-]+)/password/$', userena_views.password_change,
+        {'success_url':'/'}),
     url(r'^accounts/signup', userena_views.signup,{'success_url':'/'}),
 	url(r'^accounts/', include('userena.urls')),
 	url(r'^admin/', include(admin.site.urls)),
-	
-	#url(r'^posts/', PostView.as_view(success_url = "results")),
+		#url(r'^posts/', PostView.as_view(success_url = "results")),
 	url(r'^posts/', include('posts.urls',namespace = "post"))
+
 )
