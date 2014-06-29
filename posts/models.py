d4ed9b31d7cc8c6f3b8bdc0347894b93e4feb11c
evansam119 <b00705016@ntu.edu.tw>
2014-06-30 02:44:12 +0800

    晚上睡不著調整了一下預覽文章

--- posts/models.py
+++ posts/models.py
@@ -14,3 +14,12 @@ class Post(models.Model):
 
 	def __unicode__(self):  # Python 3: def __str__(self):
 		return  self.title
+
+class Reply(models.Model):
+
+	post = models.ForeignKey(Post)
+	context = models.CharField(max_length=500)
+	time = datetime.datetime.now()
+
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return  self.title
