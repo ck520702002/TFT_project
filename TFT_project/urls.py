6f3db4ec6dcb5735d164c9ba4922757384672e9b
TingYuLiu <az811219@hotmail.com.tw>
2014-07-07 19:58:04 +0800

    \連結完成~~唷呼~~/

--- TFT_project/urls.py
+++ TFT_project/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from posts.views import PostView,ShowPost
 from userena import views as userena_views
 from views import IndexView
+from TFT_project.views import TeacherInfoOneOnOneView
+from TFT_project.views import TeacherInfoNwMentorView
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,7 +15,8 @@ urlpatterns = patterns('',
     url(r'^accounts/signup', userena_views.signup,{'success_url':'/'}),
 	url(r'^accounts/', include('userena.urls')),
 	url(r'^admin/', include(admin.site.urls)),
-    url(r'^(?P<page_name>about|faq|press|whatever)/$', 'accounts.teacher', name='teacher_info'),
+    url(r'^teacher/oneonone', TeacherInfoOneOnOneView.as_view()),
+    url(r'^teacher/nwmentor', TeacherInfoNwMentorView.as_view()),
 
 	#url(r'^posts/', PostView.as_view(success_url = "results")),
 	url(r'^posts/', include('posts.urls',namespace = "post")),
