51b851846a878fdaf7a33b7fbadc483fe8809b8e
TingYuLiu <az811219@hotmail.com.tw>
2014-07-14 15:28:56 +0800

    QAMQAM

--- posts/views.py
+++ posts/views.py
@@ -14,8 +14,21 @@ class PostView(CreateView,ListView):
 
 class ShowPost(CreateView,ListView):
 	model = Post
+	#context_object_name = 'posts'
+	queryset = Post.objects.all()
 	template_name = 'discuss.html'
-
+	'''def get_queryset(self):
+        self.post = get_object_or_404(Post, name=self.args[0])
+        return Post.objects.filter(post=self.post)'''
+	def get_context_data(self, **kwargs):
+    # Call the base implementation first to get a context
+		context = super(ShowPost, self).get_context_data(**kwargs)
+    # Add in the publisher
+		documents = []
+		for document in self.queryset:
+			documents.insert(0, document)
+		context['posts'] = documents
+		return context
 class PostDetail(DetailView):
 	model = Post
 	template_name = 'post_detail.html'
